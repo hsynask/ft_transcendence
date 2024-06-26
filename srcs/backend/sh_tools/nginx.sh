f02581c2231634fefd6d33e904534b68ca9027ca
Cemal Bolat <cbolat@k2m13s09.42kocaeli.com.tr>
2024-06-26 16:39:32 +0300

    done

--- srcs/backend/sh_tools/nginx.sh
+++ srcs/backend/sh_tools/nginx.sh
@@ -1,7 +1,8 @@
-
 #!/bin/bash
 
-openssl req -newkey rsa:2048 -nodes -keyout $CERTIFICICATES_KEYOUT -x509 -days 365 \
--out $CERTIFICICATES_OUT -subj "/C=TR/ST=KOCAELI/L=GEBZE/O=42Kocaeli/CN=$DOMAIN_NAME";
+# Generate self-signed SSL certificate
+openssl req -newkey rsa:2048 -nodes -keyout "$CERTIFICICATES_KEYOUT" -x509 -days 365 \
+    -out "$CERTIFICICATES_OUT" -subj "/C=TR/ST=KOCAELI/L=GEBZE/O=42Kocaeli/CN=$DOMAIN_NAME"
 
-exec "$@"
+# Start nginx in the foreground with daemon off
+nginx -g "daemon off;"
